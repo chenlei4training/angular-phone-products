83df7ca180969b77f33b416cc9645776f672bc63
chenlei4training <2624304501@qq.com>
2021-01-21 17:19:04 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-s3ricb-hhnpbn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-s3ricb-hhnpbn)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: '普通手机',
+    price: 299,
+    description: ''
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
